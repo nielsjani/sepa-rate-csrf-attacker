d47d0262a7574fecfe79d3519d3006913a366702
nielsjani <NielsJani>
2019-02-08 16:01:38 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'csrf-attacker';
+export class AppComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    // this.httpClient.get('http://localhost:8080/get/attack', {withCredentials: true})
+    //   .subscribe(() => console.log('The damage has been done...'));
+  }
 }
